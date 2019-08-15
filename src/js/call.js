701fc16b162c2f11e660fd0705aab219bc301087
sadmaloy <rostislav_ivanov2015@mail.ru>
2019-08-16 02:10:50 +0300

    Добавил модальное окно для кнопки перезвоните мне

--- src/js/call.js
+++ src/js/call.js
@@ -1,13 +1,13 @@
-$(document).ready(function(){
+$(document).ready(function() {
     var button = $('#button');
-    var modal = $('#call');
+    var call = $('#call');
     var close = $('#close-call');
 
     button.on('click', function() {
-        modal.addClass('call_active');
+        call.addClass('call_active');
     });
 
     close.on('click', function() {
-        modal.removeClass('call_active');
+        call.removeClass('call_active');
     });
 });
